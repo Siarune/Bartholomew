3635980a1da67b1c94b73c9a223f89f52fcd037c
Aidan Sharp <enigma0fox@gmail.com>
2021-07-09 21:14:36 -0400

    env change

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+/*
+
+// This line MUST be first, for discord.js to read the process envs!
+require('dotenv').config(); 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  // The process.env.PREFIX is your bot's prefix in this case.
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+  // This is the usual argument parsing we love to use.
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // And our 2 real basic commands!
+  if(command === 'ping') {
+    message.channel.send('Pong!');
+  } else
+  if (command === 'blah') {
+    message.channel.send('Meh.');
+  }
+});
+
+// There's zero need to put something here. Discord.js uses process.env.CLIENT_TOKEN if it's available,
+// and this is what is being used here. If on discord.js v12, it's DISCORD_TOKEN
+client.login();
+*/
+
+
+require('dotenv').config();
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token, readyMessage } = require('./config.json');
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
@@ -12,13 +47,13 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-    console.log(readyMessage);
+    console.log(process.env.READYMESSAGE);
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (!client.commands.has(command)) return;
@@ -28,8 +63,20 @@ client.on('message', message => {
         console.log(command);
     } catch (error) {
         console.error(error);
-        message.reply('Some shit fucked up');
+        message.reply(process.env.ERRORMESSAGE);
     }
 });
 
-client.login(token);
+client.on('guildMemberAdd', (member) => {
+    const guild = member.guild;
+    console.log('new person');
+    guild.channels.cache.find(channel => channel.name === "general").send('welcome idiot');
+  });
+
+  client.on('guildMemberRemove', (member) => {
+    const guild = member.guild;
+    console.log('person left');
+    guild.channels.cache.find(channel => channel.name === "general").send('bye bitch');
+});
+
+client.login();
