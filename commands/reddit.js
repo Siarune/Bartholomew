890c83385792eb3a22fa72d036f8670cf2ab99f9
Aidan Sharp <enigma0fox@gmail.com>
2021-08-31 15:26:35 -0400

    revamped modules

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,105 +1,72 @@
-// Version 1.0
-/*
 module.exports = {
 	name: 'reddit',
 	description: 'Pull a picture from any subreddit',
 	execute(message, args) {
 
 		const Discord = require("discord.js");
-		const axios = require("axios");
-		const cheerio = require("cheerio");
-		const subreddit = args;
+		const fetch = (...args) => import('node-fetch').then(({
+			default: fetch
+		}) => fetch(...args));
+		const linkBase = "https://www.reddit.com/r/"
+		
 
-		axios
-			.get(`https://reddit.com/r/` + subreddit)
-			.then((response) => {
-				dealWithData(response.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		const get = () => {
+			fetch(linkBase + args + "/hot.json")
+				.then(function (res) {
+					return res.json(); // Convert the data into JSON
+				})
+				.then(function (res) {
+					let i = 0;
+					let postsArr = res.data.children;
+					let currPost = postsArr[i].data;
+					let img = "hi";
+					shift();
+					media();
 
-		const dealWithData = (html) => {
-			const $ = cheerio.load(html);
-			const urlMeme = $("._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax");
-			const urlTitle = $("._2SdHzo12ISmrC8H86TgSCp._3wqmjmv3tb_k-PROt7qFZe ");
-			const postClass = $("._1oQyIsiPHYt6nx7VOmd1sz._1RYN-7H8gYctjOQeL8p2Q7.scrollerItem._3Qkp11fjcAw9I9wtLo8frE._1qftyZQ2bhqP62lbPjoGAh.Post.t3_omudhx");
+					function shift() {
+						if (currPost.stickied == true || currPost.is_created_from_ads_ui == true) {
+							i++;
+							currPost = postsArr[i].data;
+							shift();
+						} else {
+							return;
+						}
+					}
 
+					function media() {
+						if (!currPost.secure_media || currPost.secure_media == null) {
+							return;
+						} else {
+							img = currPost.secure_media.reddit_video.scrubber_media_url;
+							console.log(img);
+						}
+					}
 
-			const indexValue = randNo(urlMeme.length * 0.67);
-			const img = `${urlMeme[indexValue].attribs.src}`;
-			const title = $(urlTitle[indexValue + 2]).text(); //Need to make a dynamic offset because of pins
+					const title = currPost.title;
+					const text = currPost.selftext;
+					const author = currPost.author;
+					const link = currPost.permalink;
+					const sub = currPost.subreddit;
+					const upvotes = currPost.ups;
+					const downvotes = currPost.downs;
+					const awards = currPost.total_awards_received;
 
-			const Embed = new Discord.MessageEmbed()
-				.setTitle(title)
-				.setColor("#042069")
-				//.setImage(img);
+					const Embed = new Discord.MessageEmbed()
+						.setTitle(title)
+						.setURL("https://reddit.com" + link)
+						//.setThumbnail(thumbnail)
+						.setDescription(text)
+						.setColor("#042069")
+						//.setImage(img)
+						.setFooter("Posted by " + author + " on " + sub);
 
-			message.channel.send(Embed);
-
-		};
-
-		const randNo = (limit) => {
-			const thatNo = Math.floor(Math.random() * limit);
-			return thatNo;
-		};
-	},
-};
-*/
-
-//Version 2.0
-
-module.exports = {
-	name: 'reddit',
-	description: 'Pull a picture from any subreddit',
-	execute(message, args) {
-
-		const Discord = require("discord.js");
-		const axios = require("axios");
-		const cheerio = require("cheerio");
-		const subreddit = args;
-
-		axios
-			.get(`https://reddit.com/r/` + subreddit)
-			.then((response) => {
-				dealWithData(response.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-
-		const dealWithData = (html) => {
-			const $ = cheerio.load(html);
-
-			const thumbnailClass = $(".Mh_Wl6YioFfBc9O1SQ4Jp");
-			const postClass = $("._1oQyIsiPHYt6nx7VOmd1sz._1RYN-7H8gYctjOQeL8p2Q7.scrollerItem._3Qkp11fjcAw9I9wtLo8frE._1qftyZQ2bhqP62lbPjoGAh.Post");
-			const titleClass = $("._eYtD2XCVieq6emjKBH3m");
-			const imgClass = $("._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax");
-			const authorClass = $("._2mHuuvyV9doV3zwbZPtIPG");
-			const timeClass = $("._3jOxDPIQ0KaOWpzvSQo-1s");
-
-			const indexValue = randNo(postClass.length * 0.67);
-			const post = $(postClass[indexValue]);
-			const title = $(post).find(titleClass).text().trim();
-			const img = $(post).find(imgClass).attr("src");
-			const author = $(post).find(authorClass).text();
-			const time = $(post).find(timeClass).text();
-			const thumbnail = $(thumbnailClass).attr("src");
-
-			const Embed = new Discord.MessageEmbed()
-				.setTitle("Here's you meme")
-				.setThumbnail(thumbnail)
-				.setDescription(title)
-				.setColor("#042069")
-				.setImage(img)
-				.setFooter(author);
-
-			message.channel.send(Embed);
-		};
-
-		const randNo = (limit) => {
-			const thatNo = Math.floor(Math.random() * limit);
-			return thatNo;
-		};
+					message.channel.send(Embed);
+				})
+				.catch(function (err) {
+					console.log(err); // Log error if any
+				});
+				
+		}
+get();
 	},
 };
--- index.js
+++ index.js
@@ -2,7 +2,45 @@ require('dotenv').config();
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const welcome = require('./welcome.js')
+const welcome = require('./welcome.js');
+
+const Log = {
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  dim: "\x1b[2m",
+  underscore: "\x1b[4m",
+  blink: "\x1b[5m",
+  reverse: "\x1b[7m",
+  hidden: "\x1b[8m",
+  // Foreground (text) colors
+  fg: {
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+    crimson: "\x1b[38m"
+  },
+  // Background colors
+  bg: {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m",
+    crimson: "\x1b[48m"
+  }
+};
+
+const log = (color, text) => {
+  console.log(`${color}%s${Log.reset}`, text);
+};
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,7 +51,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-  console.log(process.env.READYMESSAGE);
+  log(Log.fg.green, process.env.READYMESSAGE);
   welcome(client);
 
   client.user.setPresence({
@@ -23,6 +61,7 @@ client.once('ready', () => {
       status: 'online'
     })
     .catch(console.error);
+
 });
 
 client.on('message', message => {
@@ -41,30 +80,6 @@ client.on('message', message => {
     message.reply(process.env.ERRORMESSAGE);
   }
 });
-
-/*
-const welcomeChannel = process.env.WELCOMECHANNEL;
-client.on("guildMemberAdd", (member) => {
-
-    console.log("New moron here");
-
-    const message = `Welcome <@${member.id}> to our mental support hellserver!`;
-    const channel = member.guild.channels.cache.get(welcomeChannel);
-
-    channel.send(message);
-});
-
-client.on("guildMemberRemove", (member) => {
-
-    console.log("Some moron left");
-
-    const message = `Goodbye <@${member.id}>, and good riddance!`;
-    const channel = member.guild.channels.cache.get(welcomeChannel);
-
-    channel.send(message);
-    
-});
-*/
 
 client.login();
 client.on("error", (e) => console.error(e));
